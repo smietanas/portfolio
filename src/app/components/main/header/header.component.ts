4bac6fe71ba70b690d4ab769b2f54c774f18c3ee
Krzysiek <ksmiertka@gmail.com>
2021-02-12 12:29:42 +0100

    add scroll spy

--- src/app/components/main/header/header.component.ts
+++ src/app/components/main/header/header.component.ts
@@ -1,4 +1,4 @@
-import {AfterContentInit, Component } from '@angular/core';
+import { AfterContentInit, Component } from '@angular/core';
 import { gsap } from 'gsap';
 declare var $: any;
 @Component({
@@ -10,46 +10,64 @@ export class HeaderComponent implements AfterContentInit {
   items: any;
   ul: any;
 
-  constructor() {}
+  constructor() {
+    $(document).ready(function () {
+      const scrollLink = $('.scroll');
+      // Smooth scrolling
+      scrollLink.click(function (e) {
+        e.preventDefault();
+        $('body,html').animate({
+          scrollTop: $(this.hash).offset().top - 100
+        }, 1000);
+      });
+      // Active link switching
+      $(window).scroll(function () {
+        let scrollbarLocation = $(this).scrollTop();
+        scrollLink.each(function () {
+          let sectionOffset = $(this.hash).offset().top - 200;
+          if (sectionOffset <= scrollbarLocation) {
+            $(this).parent().addClass('active');
+            $(this).parent().siblings().removeClass('active');
+          }
+        })
+      })
+    })
+  }
 
   ngAfterContentInit(): void {
+
+
     this.items = document.querySelectorAll('.nav-link');
     this.ul = document.querySelector('.navbar-nav');
 
     $('.navbar-nav>li>a').on('click', function () {
       $('.navbar-collapse').collapse('hide');
     });
+
   }
 
   setActiveClass(e: MouseEvent) {
     const elem = e.target as HTMLElement;
-    
-    if (elem.classList.contains('nav-link')) {
-      this.items.forEach((item: HTMLElement) => item.classList.remove("active"));
-      elem.classList.add("active")
-    }
     const pageX = e.pageX;
-    const rotate = pageX -10 - window.innerWidth/2;
+    const rotate = pageX - 10 - window.innerWidth / 2;
     gsap.to(this.ul, { rotationX: -Math.abs(0.1 * rotate), rotationY: 0.2 * rotate, rotationZ: -0.1, duration: 0.3 });
     gsap.to(this.ul, { rotationX: 0, rotationY: 0, rotationZ: 0, duration: 0.2, delay: 0.2 });
 
     gsap.to(elem, { scale: .8, ease: "Power4.easeOut", duration: 0.3 });
     gsap.to(elem, { scale: 1, ease: "Back.easeOut.config(4)", delay: 0.2 });
-
-    let destinationId = `s${elem.id}`
-    console.log(destinationId);
-    const dd = document.getElementById(destinationId);
-    console.log(dd);
-    const toTop = 120;
-    $('html, body').animate({
-      scrollTop: $(dd).offset().top-toTop
-    }, 1000)
-
-
   }
 
 
-  smoothScroll(id){
- 
-  }
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProjectsModel } from 'src/app/shared/models/ProjectsModel';
 import { HttpService } from 'src/app/shared/services/http.service';
 
@@ -10,38 +10,35 @@ import { HttpService } from 'src/app/shared/services/http.service';
 export class ProjectsComponent implements OnInit {
   menuItems: any;
   projectList: ProjectsModel[];
-  item :ProjectsModel;
-  constructor(private service : HttpService) { }
+  item: ProjectsModel;
+  constructor(private service: HttpService) { }
 
 
   ngOnInit(): void {
 
     this.menuItems = document.querySelectorAll('.menu__item');
-    
-    this.service.getByProjectCategory('all').subscribe(data => {      
-      this.projectList = data;        
+
+    this.service.getByProjectCategory('all').subscribe(data => {
+      this.projectList = data;
     });
   }
 
-  showMore(id:number){
-    this.service.getProjectByID(id).subscribe(data =>{
+  showMore(id: number) {
+    this.service.getProjectByID(id).subscribe(data => {
       this.item = data[0];
     })
   }
-  filterProject(e:MouseEvent) {
+  filterProject(e: MouseEvent) {
     const target = e.target as HTMLElement;
     const dataFilter = target.getAttribute('data-filter');
 
     if (dataFilter) {
-      this.service.getByProjectCategory(dataFilter).subscribe(data => { 
-        this.projectList = data; 
-      
+      this.service.getByProjectCategory(dataFilter).subscribe(data => {
+        this.projectList = data;
       });
-      
 
-       this.menuItems.forEach((item: HTMLElement) => item.classList.remove("active"));
-       target.classList.add("active") 
-       
+      this.menuItems.forEach((item: HTMLElement) => item.classList.remove("active"));
+      target.classList.add("active")
     }
   }
 
